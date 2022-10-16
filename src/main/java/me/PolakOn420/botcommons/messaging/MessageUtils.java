e3cb0b0f517f77060eb0b18a2938227e01c7f866
PolakTheRealOne <40703633+MythicZYT@users.noreply.github.com>
2022-10-17 00:00:46 +0200

    Update MessageUtils.java

--- src/main/java/me/PolakOn420/botcommons/messaging/MessageUtils.java
+++ src/main/java/me/PolakOn420/botcommons/messaging/MessageUtils.java
@@ -1,22 +1,6 @@
-/*
- *    Copyright 2018 Duncan "duncte123" Sterken
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
+package me.PolakOn420.botcommons.messaging;
 
-package me.duncte123.botcommons.messaging;
-
-import me.duncte123.botcommons.commands.ICommandContext;
+import me.PolakOn420.botcommons.commands.ICommandContext;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.MessageBuilder;
