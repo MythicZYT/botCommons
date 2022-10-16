1256eb9e7d06d65984e9731149ab2541d8e54e67
PolakTheRealOne <40703633+MythicZYT@users.noreply.github.com>
2022-10-16 23:51:43 +0200

    Create DefaultCommandContext.java

---
+++ src/main/java/me/PolakOn420/botcommons/commands/DefaultCommandContext.java
@@ -0,0 +1,33 @@
+
+
+package me.PolakOn420.botcommons.commands;
+
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.internal.utils.Checks;
+
+import java.util.List;
+
+/**
+ * Provides a basic command context that should be sufficient for most bots
+ */
+public class DefaultCommandContext implements ICommandContext {
+    private final GuildMessageReceivedEvent event;
+    private final List<String> args;
+
+    public DefaultCommandContext(List<String> args, GuildMessageReceivedEvent event) {
+        Checks.notNull(event, "event");
+        Checks.notNull(args, "args");
+
+        this.args = args;
+        this.event = event;
+    }
+
+    public List<String> getArgs() {
+        return this.args;
+    }
+
+    @Override
+    public GuildMessageReceivedEvent getEvent() {
+        return this.event;
+    }
+}
