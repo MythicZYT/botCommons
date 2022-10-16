8241668f44b723bf8d1a067184fefffd158dd907
PolakTheRealOne <40703633+MythicZYT@users.noreply.github.com>
2022-10-17 00:02:47 +0200

    Update WebParserUtils.java

--- src/main/java/me/PolakOn420/botcommons/web/WebParserUtils.java
+++ src/main/java/me/PolakOn420/botcommons/web/WebParserUtils.java
@@ -1,27 +1,11 @@
-/*
- *    Copyright 2018 Duncan "duncte123" Sterken
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
-package me.duncte123.botcommons.web;
+package me.PolakOn420.botcommons.web;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.natanbc.reliqua.request.RequestContext;
 import com.github.natanbc.reliqua.request.RequestException;
-import me.duncte123.botcommons.JSONHelper;
+import me.PolakOn420.botcommons.JSONHelper;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
